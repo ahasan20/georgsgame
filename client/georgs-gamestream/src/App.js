aba015f9758d52da9989b16f828385445a15b54a
alexzhang13 <alex.lx.zhang@gmail.com>
2021-02-12 23:18:04 -0500

    Add basic firestone features

--- client/georgs-gamestream/src/App.js
+++ client/georgs-gamestream/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
+import { useFirestoreDocData, useFirestore, SuspenseWithPerf } from 'reactfire';
 import './App.css';
+
+const firebase = require("firebase");
+require("firebase/firestore");
+
+function Game() {
+  // lazy load the Firestore SDK and create a document reference
+  const gameRef = useFirestore()
+    .collection('6oMC55R6lnniZFzKn1UN')
+    .doc('game');
+
+  // subscribe to the doc. just one line!
+  const game = useFirestoreDocData(gameRef);
+
+  // get the value from the doc
+  const state = game.player_turn;
+
+  return <p>The game state is {state ? "qtop" : "qbot"}</p>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*
+        SuspenseWithPerf behaves the same as Suspense,
+        but also automatically measures load times with the User Timing API
+        and reports it to Firebase Performance Monitoring
+      */}
+      <SuspenseWithPerf fallback={'loading game status...'} traceId={'load-game-status'}>
+        <Game />
+      </SuspenseWithPerf>
     </div>
   );
 }
--- client/georgs-gamestream/src/index.js
+++ client/georgs-gamestream/src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FirebaseAppProvider } from 'reactfire';
 
-ReactDOM.render(
+const firebaseConfig = {
+  apiKey: "AIzaSyDtKtCjmDGio_3jsEqsI0cIleqndvD4pJg",
+  authDomain: "georgsgame-3d44e.firebaseapp.com",
+  projectId: "georgsgame-3d44e",
+  storageBucket: "georgsgame-3d44e.appspot.com",
+  messagingSenderId: "768069468543",
+  appId: "1:768069468543:web:2b5b620699311106775a4a"
+};
+
+ReactDOM.unstable_createRoot(document.getElementById('root')).render(
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <App />
+  </FirebaseAppProvider>
+);
+
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
