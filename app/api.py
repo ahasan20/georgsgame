d3000fb2e6db692e8d8add749e348f356cb9e302
Areeq Hasan <areeq.i.hasan@gmail.com>
2021-02-12 22:43:54 -0500

    georgs-gamestream

--- app/api.py
+++ app/api.py
@@ -11,13 +11,15 @@ from app import app
 def version():
     return json.dumps(qiskit.__qiskit_version__, indent=4, sort_keys=True, default=str)
 
-@app.route("/api/simulate")
-def test_simulator():
-    gate = request.args.get('gate')
-    qc = QuantumCircuit(1, 1)
-    if gate == 'X': qc.x(0)
-    qc.measure(0, 0)
-    return json.dumps(execute(qc, Aer.get_backend('qasm_simulator')).result().get_counts(), indent=4, sort_keys=True, default=str)
+@app.route("/api/get/probabilities")
+def get_probabilities():
+    state = request.args.get('state')
+    # translate state --> circuit
+    return 
+    
+@app.route("/api/get/measurement")
+def get_measurement():
+    state = request.args.get('state')
 
 @app.route('/')
 def index():
