7741b0409041124551495f6c3c80d2c84c35871f
Areeq Hasan <areeq.i.hasan@gmail.com>
2021-02-13 12:12:22 -0500

    drill

--- app/api.py
+++ app/api.py
@@ -15,15 +15,15 @@ def version():
 @app.route("/api/get/probabilities", methods=['GET', 'POST'])
 def get_probabilities_api():
     print(request.json)
-    return json.dumps(get_probabilities(state_to_circuit(request.json.get('state'))), indent=4, sort_keys=True, default=str)
+    return json.dumps(get_probabilities(state_to_circuit(request.json['data']['state'])), indent=4, sort_keys=True, default=str)
 
 @app.route("/api/get/measurement", methods=['GET', 'POST'])
 def get_measurement_api():
-    return json.dumps(get_measurement(state_to_circuit(request.json.get('state'))), indent=4, sort_keys=True, default=str)
+    return json.dumps(get_measurement(state_to_circuit(request.json['data']['state'])), indent=4, sort_keys=True, default=str)
 
 @app.route("/api/get/statevector", methods=['GET', 'POST'])
 def get_statevector_api():
-    return json.dumps(get_statevector(state_to_circuit(request.json.get('state'))), indent=4, sort_keys=True, default=str)
+    return json.dumps(get_statevector(state_to_circuit(request.json['data']['state'])), indent=4, sort_keys=True, default=str)
 
 @app.route('/')
 def index():
