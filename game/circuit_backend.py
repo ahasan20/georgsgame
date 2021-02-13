d005ee690b45036bdd0f97d3e86a1cc39b6c34b4
georgezhou20 <geoyzhou@gmail.com>
2021-02-12 22:54:18 -0500

    Added a dictionary with keys as string and values as Gates

--- game/circuit_backend.py
+++ game/circuit_backend.py
@@ -1,12 +1,25 @@
 from qiskit import QuantumCircuit, Aer, execute, QuantumRegister, ClassicalRegister
+from qiskit.circuit.library import HGate, XGate, YGate, ZGate, CXGate, CYGate, CZGate, CHGate, SwapGate
 import numpy as np
+
+gate_dict = {
+    'H' : HGate(),
+    'X' : XGate(),
+    'Y' : YGate(),
+    'Z' : ZGate(),
+    'CX' : CXGate(),
+    'CY' : CYGate(),
+    'CZ' : CZGate(),
+    'CH' : CHGate(),
+    'SW' : SwapGate()
+}
 
 def list_to_circuit(gates):
     qr = QuantumRegister(2)
     cr = ClassicalRegister(2)
     qc = QuantumCircuit(qr, cr)
     for gate in gates:
-        qc.append(gate[0], qargs=gate[1])
+        qc.append(gate_dict[gate[0]], qargs=gate[1])
     return qc
 
 def get_statevector(qc):
